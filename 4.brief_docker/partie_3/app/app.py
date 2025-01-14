6d3627e9f47c0c0d994871aab529c45921422840
florian <berthelot_florian@yahoo.fr>
2025-01-14 10:32:05 +0100

    fin docker

--- 4.brief_docker/partie_3/app/app.py
+++ 4.brief_docker/partie_3/app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Configuration de la connexion à la base de données MariaDB
 DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_NAME = os.getenv('DB_NAME', 'mydb')
+DB_NAME = os.getenv('DB_NAME', 'db')
 DB_USER = os.getenv('DB_USER', 'user')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
 
--- 4.brief_docker/partie_3/etl/etl.py
+++ 4.brief_docker/partie_3/etl/etl.py
@@ -5,7 +5,7 @@ import sqlalchemy
 df_clients = pd.read_csv('csv/clients.csv')
 
 DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_NAME = os.getenv('DB_NAME', 'mydb')
+DB_NAME = os.getenv('DB_NAME', 'db')
 DB_USER = os.getenv('DB_USER', 'root')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'root')
 DB_PORT = os.getenv('DB_PORT', '3306')
